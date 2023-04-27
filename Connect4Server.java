20902bd6658b8cac01b0028406033987c594e9e7
LucasSM3006 <sasso3006@gmail.com>
2023-04-27 16:19:38 -0300

    Adding files. Only file with a purpose at the moment is 'connect4server', as it has the main function. Remember to divide everything correctly after finishing the logic.

---
+++ Connect4Server.java
@@ -0,0 +1,111 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Connect4Server {
+    public static final int ROWS = 7;
+    public static final int COLUMNS = 6;
+    public static final char YELLOW = 'Y';
+    public static final char BLUE = 'B';
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        final int[][] board = new int[ROWS][COLUMNS];
+
+        for(int i = 0; i < ROWS; i++) {
+            for(int j = 0; j < COLUMNS; j++) {
+                board[i][j] = 0;
+            }
+        }
+//        String b = "";
+
+        boolean gameRunning = true;
+
+        while(gameRunning) {
+            buildBoard(board);
+
+            System.out.print("Provide the row (1 to 7) to put a number in or type 8 to end the game: ");
+            int chosen_row = scanner.nextInt();
+            System.out.println("Input: " + chosen_row);
+
+            if (chosen_row == 8) {
+                System.out.println("Ending game.");
+                break;
+            }
+            putIntoBoard(board, chosen_row, 1);
+
+        }
+
+
+//        switch(chosen_row) {
+//            case 1:
+//
+//                break;
+//            case 2:
+//
+//                break;
+//            default:
+//        }
+
+//        for (int i = 0; i < COLUMNS; i++) {
+//            for(int j = 0; j < ROWS; j++) {
+//                b += "|" + board[j][i];
+//            }
+//            b += "|\n";
+//        }
+
+//        System.out.println("Value at row 4, column 1: " + board[3][0]);
+//        System.out.println("Value at row 2, column 4: " + board[1][3]);
+
+//        System.out.println(b);
+
+
+
+//        System.out.println("Rows: " + board.length);
+//        System.out.println("Columns: " + board[0].length);
+
+
+    }
+
+    public static void buildBoard(int[][] board) {
+        String b = "";
+
+        for (int i = 0; i < COLUMNS; i++) {
+            for(int j = 0; j < ROWS; j++) {
+                b += "|";
+                if(board[j][i] == 0) {
+                    b += " ";
+                } else if(board[j][i] == 1) {
+                    b += YELLOW;
+                } else if(board[j][i] == 2) {
+                    b += BLUE;
+                }
+            }
+            b += "|\n";
+        }
+        System.out.println(b);
+    }
+
+    public static int[][] putIntoBoard(int[][] board, int row, int value) {
+        row = row - 1;
+
+        if(board[row][0] != 0) { // If the first value of the row is anything other than zero, then there is no need to run the loop.
+            System.out.println("Row is full. Pick another.");
+        } else {
+            for(int i = (COLUMNS - 1); i >= 0; i--) {
+                if(board[row][i] == 0) {
+                    board[row][i] = value;
+                    break;
+                }
+            }
+            System.out.println("Placed value into row.");
+        }
+
+        return board;
+    }
+
+    public static void winCheck(int[][] board) {
+
+    }
+}
---
+++ GameBoard.java
@@ -0,0 +1,6 @@
+public class GameBoard {
+    private final int VERTICAL_SIZE = 6;
+    private final int HORIZONTAL_SIZE = 7;
+
+
+}
