5e5d49fe7f2edaaae00d42208ea6fc8d389fcb74
LucasSM3006 <sasso3006@gmail.com>
2023-05-13 17:19:36 -0300

    Interface for the server. Same deal as before.

--- Connect4.java
+++ Connect4.java
@@ -2,11 +2,13 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Connect4 extends Remote {
+
     public static final String URL_SERVER = "rmi://127.0.0.1/Connect4";
 
-    void registrar(Player jogador) throws RemoteException;
+    void register(Player player) throws RemoteException;
 
-    boolean marcar(int linha, int coluna) throws RemoteException;
+    boolean mark(int coluna) throws RemoteException;
 
-    void encerrarTurno(Symbols simbolo) throws RemoteException;
+    void endTurn(Symbol symbol) throws RemoteException;
+
 }
