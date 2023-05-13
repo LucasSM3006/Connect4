1a4e91b6e7256b45c2c5697319ae7f3e5ece2d12
LucasSM3006 <sasso3006@gmail.com>
2023-05-13 17:21:33 -0300

    Interface for the players. Follows pretty much the same logic as the example.

--- Player.java
+++ Player.java
@@ -2,11 +2,11 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Player extends Remote {
-    public void aguardarOponente() throws RemoteException;
+    public void waitForOpponent() throws RemoteException;
 
-    public void iniciarPartida(Tabuleiro tabuleiro, Symbols simbolo) throws RemoteException;
+    public void startMatch(Board board, Symbol symbol) throws RemoteException;
 
-    public void iniciarTurno(Tabuleiro tabuleiro) throws RemoteException;
+    public void beginTurn(Board board) throws RemoteException;
 
-    public void encerrarPartida(Tabuleiro tabuleiro) throws RemoteException;
+    public void endMatch(Board board) throws RemoteException;
 }
